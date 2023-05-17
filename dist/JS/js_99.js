775ba59803ef915384a4baac12fa94c1d6130917
origin <archer10587@gmail.com>
2023-05-17 21:01:55 +0800

    99運算

--- dist/JS/js_99.js
+++ dist/JS/js_99.js
@@ -1,7 +1,28 @@
 const genTable = () => {
-    console.log('123');
+    let n1 = document.querySelector('#n1');
+    let n2 = document.querySelector('#n2');
+
+
+    // console.log(n1, n2);
+    // console.log(n1.value, n2.value)
+
+    if (!n1) {
+        return;
+    }
+
+    if (n1.value <= 0) {
+        return;
+    }
+
+    if (n2.value <= 0) {
+        return;
+    }
+
+    let result = [];
+    for (let i = 1; i <= n1.value; i++) {
+        for (let j = 1; j <= n2.value; j++); {
+            result.push('${i} *${j}=${i*j}');
+        }
+    }
+    console.log(result);
 }
-
-let make = document.querySelector('#make');
-
-make.addEventListener('click', genTable);
