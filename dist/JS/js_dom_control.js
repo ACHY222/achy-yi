0dbec92df6f724e404a9552b640042eadc3025f1
origin <archer10587@gmail.com>
2023-05-22 21:28:16 +0800

    手機選單icon操作

---
+++ dist/JS/js_dom_control.js
@@ -0,0 +1,32 @@
+let uid = document.querySelector('#uid');
+
+let doit = document.querySelector('#doit');
+
+doit.addEventListener('click', () => {
+    uid.style.color = 'red';
+    uid.style.fontSize = '100px';
+    uid.style.backgroundColor = 'yellow';
+});
+
+let addClass = document.querySelector('#add-class');
+
+addClass.addEventListener('click', () => {
+    uid.classList.add('active');
+});
+
+let removeClass = document.querySelector('#remove-class');
+
+removeClass.addEventListener('click', () => {
+    uid.classList.remove('active');
+});
+
+let mobileIcon = document.querySelector('#mobile-icon');
+
+
+mobileIcon.addEventListener('click', () => {
+    if (mobileIcon.classList.contains('active')) {
+        mobileIcon.classList.remove('active');
+    } else {
+        mobileIcon.classList.add('active');
+    }
+});
