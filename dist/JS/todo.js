5214c5f53fd3ba55a820c3203760e6dd85030f1f
origin <archer10587@gmail.com>
2023-05-31 19:53:22 +0800

    按鈕與輸入框事件綁定

--- dist/JS/todo.js
+++ dist/JS/todo.js
@@ -1,1 +1,33 @@
-// alert('todo')
+const init = () => {
+    let addBtn = document.querySelector('add-btn');
+    let item-name =document.querySelector('#item-name');
+
+    addBtn.addEventListener('click', () => {
+        let value = itemName.value;
+
+        if (!value) {
+            alert('請輸入項目名稱');
+            itemName.focus();
+            return;
+        }
+
+        console.log(value);
+    });
+
+    itemName.addEventListener('keyup', (e) => {
+        if (e.key == 'Enter') {
+            let value = itemName.value;
+
+
+            if (!value) {
+                alert('請輸入項目名稱');
+                itemName.focus();
+                return;
+            }
+
+            console.log(value);
+        }
+    });
+};
+
+init();
