bdab91c8f04dba784167b4e4a791f4659fc85d49
origin <archer10587@gmail.com>
2023-05-31 21:02:22 +0800

    vue 引入

--- dist/JS/todo.js
+++ dist/JS/todo.js
@@ -11,12 +11,35 @@ const init = () => {
                 title: '新增錯誤',
                 text: '請輸入項目名稱',
                 icon: 'error',
+            }).then(() => {
+                setTimeout(() => {
+                    itemName.focus();
+                }, 500)
             })
-            itemName.focus();
             return;
         }
 
-        console.log(value);
+        let ul = document.querySelector('#pending-item');
+
+        // 正規作法
+        // let li =document.createElement('li');
+        // let checkbox = document.createElement('input');
+        // let label =document.createElement('label');
+        // checkbox.type ='checkbox';
+        // label.innerHTML = value;
+        // li.appendChild(checkbox);
+        // li.appendChild(label);
+
+        // ul.appendChild(li);
+
+        // 簡單作法
+
+        ul.innerHTML += `
+        <li>
+        <input type="checkbox">
+        <label for="">${value}</label>
+        </li>
+        `;
     }
 
     addBtn.addEventListener('click', () => {
