06a7a341599a50baa180ee3b9d79885c691ab0dc
origin <archer10587@gmail.com>
2023-06-05 20:01:23 +0800

    完成清單資料對應

--- dist/JS/todo_vue.js
+++ dist/JS/todo_vue.js
@@ -22,7 +22,15 @@ Vue.createApp({
                 return;
             }
             this.pending.push(value);
-            console.table(this.pending);
+            this.itemValue = '';
+            this.$refs.itemValue.focus();
+        },
+        toDone(index) {
+            let value = this.pending[index];
+
+            console.log(index, value);
+            // 把代辦項目搬移到已完成資料內
+            // 刪除目前代辦項目
         }
     }
 }).mount('#app');
