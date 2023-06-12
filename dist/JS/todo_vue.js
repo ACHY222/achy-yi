d6b1ed5ff2cebdb3d02277e59f4ed3098bf2589f
origin <archer10587@gmail.com>
2023-06-12 20:23:43 +0800

    doLoadCloud 串接

--- dist/JS/todo_vue.js
+++ dist/JS/todo_vue.js
@@ -63,68 +63,88 @@ let vm = Vue.createApp({
         update() {
             database.set('todo-pending', this.pending);
             database.set('todo-done', this.done);
-        }
-    },
-    DoSaveCloud() {
-        Swal.fire({
-            title: '儲存到雲端',
-            text: '輸入UID',
-            input: 'text',
-            showCancelButton: 'true',
-            confirmButtonText: '儲存',
-            cancelButtonText: '先不要',
+        },
 
-        }).then(response => {
+        DoSaveCloud() {
+            Swal.fire({
+                title: '儲存到雲端',
+                text: '輸入UID',
+                input: 'text',
+                showCancelButton: 'true',
+                confirmButtonText: '儲存',
+                cancelButtonText: '先不要',
+
+            }).then(response => {
+                if (response.value) {
+                    let api = 'https://book.niceinfos.com/frontend/api/';
+
+                    // 使用表單物件
+                    // let form = new FormData;
+                    // form.append('action', 'todo');
+                    // form.append('uid', response.value);
+                    // form.append('data', {
+                    //     pending: this.pendding,
+                    //     done: this.done
+                    // });
+
+                    let params = {
+                        action: 'todo',
+                        uid: response.value,
+                        data: {
+                            pending: this.pending,
+                            done: this.done
+                        }
+                    }
+
+                    // console.log(params);
+                    fetch(api, {
+                        method: 'POST',
+                        body: JSON.stringify(params)
+                    }).then(response => {
+                        return response.text();
+                    }).then(text => {
+                        setTimeout(() => {
+                            let data = JSON.parse(text);
+                            console.log(data);
+                            Swal.fire({
+                                title: '儲存完畢',
+                                text: '資料已儲存',
+                                icon: 'susses'
+
+                            })
+                        }, 400);
+                    })
+
+                    Swal.fire({
+                        title: '資料儲存中',
+                        text: '請勿關閉或重尋整理視窗',
+                        showConfirmButton: false
+                    })
+                }
+            });
+        },
+        async doLoadCloud() {
+            let response = await Swal.fire({
+                title: '儲存到雲端',
+                text: '輸入UID',
+                input: 'text',
+            });
+
             if (response.value) {
                 let api = 'https://book.niceinfos.com/frontend/api/';
-
-                // 使用表單物件
-                // let form = new FormData;
-                // form.append('action', 'todo');
-                // form.append('uid', response.value);
-                // form.append('data', {
-                //     pending: this.pendding,
-                //     done: this.done
-                // });
-
-                let params = {
-                    action: 'todo',
-                    uid: response.value,
-                    data: {
-                        pending: this.pending,
-                        done: this.done
-                    }
-                }
-
-                // console.log(params);
-                fetch(api, {
-                    method: 'POST',
-                    body: JSON.stringify(params)
-                }).then(response => {
+                fetch(`${api}?action=todo&uid=${response.uid}`).then(response => {
                     return response.text();
                 }).then(text => {
-                    setTimeout(() => {
-                        let data = JSON.parse(text);
-                        console.log(data);
-                        Swal.fire({
-                            title: '儲存完畢',
-                            text: '資料已儲存',
-                            icon: 'susses'
-
-                        })
-                    }, 400);
-                })
-
-                Swal.fire({
-                    title: '資料儲存中',
-                    text: '請勿關閉或重尋整理視窗',
-                    showConfirmButton: false
+                    console.log(text);
                 })
             }
-        });
+        }
     },
+
+
     mounted() {
-        this.pending = database.get('todo-pending', [])
+        this.pending = database.get('todo-pending', []);
+        this.done = database.get('todo-done', []);
     }
 
 }).mount('#app');
