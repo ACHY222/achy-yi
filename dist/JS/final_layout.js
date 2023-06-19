58408550860f10588890b9a01904ff61a0599cf3
origin <archer10587@gmail.com>
2023-06-19 21:27:52 +0800

    單張顯示

--- dist/JS/final_layout.js
+++ dist/JS/final_layout.js
@@ -8,12 +8,29 @@ Vue.crreateApp({
 
             ],
             currentIndex: 0,
+            timer: '',
+            loopSecond: 5
         }
     },
     methods: {
         setIndex(index) {
             this.currentIndex = index;
+            this.doLoop();
+        },
+        doLoop() {
+            clearInterval(this.timer);
+            this.timer = setInterval(() => {
+                let max = this.TimeRanges.length - 1;
+                if (this.currentIndex >= max) {
+                    this.currentIndex = 0;
+                } else {
+                    this.currentIndex++;
+                }
+            }, this.loopSecond * 1000);
         }
+    },
+    mounted() {
+        this.doLoop();
     }
 
 }).mount('#app-slider');
