75ff28051f2e1ecdd22a3116125203b683937294
origin <archer10587@gmail.com>
2023-06-19 21:05:53 +0800

    背景圖片載入

--- dist/JS/final_layout.js
+++ dist/JS/final_layout.js
@@ -6,7 +6,14 @@ Vue.crreateApp({
                 { title: '第二張', subtitle: '第二子標題', bg: '/images/banner.jpeg' },
                 { title: '第三張', subtitle: '第三子標題', bg: '/images/process.jpeg' },
 
-            ]
+            ],
+            currentIndex: 0,
+        }
+    },
+    methods: {
+        setIndex(index) {
+            this.currentIndex = index;
         }
     }
+
 }).mount('#app-slider');
